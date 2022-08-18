72dce9b297540c0c47bfadb0171e931b654aaf37
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-08-17 21:25:33 -0400

    Added console statements to explain function calls

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,5 +1,7 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
 function listTopics() {
     for (var i = 0; i < topics.length; i++) {
         console.log(topics[i]);
@@ -13,15 +15,21 @@ for(var i = 0; i < topics.length; i++) {
 */
 
 function selectTopic() {
-    if (topics[0] === 'HTML') {
+    if (randomTopic === 'HTML') {
       console.log("Let's study HTML!");
-    } else if (topics[0] === 'CSS') {
+    } else if (randomTopic === 'CSS') {
       console.log("Let's study CSS!");
-    } else if (topics[0] === 'Git') {
+    } else if (randomTopic === 'Git') {
       console.log("Let's study Git!");
-    } else if (topics[0] === 'JavaScript') {
+    } else if (randomTopic === 'JavaScript') {
       console.log("Let's study JavaScript!");
     } else {
       console.log('Please try again!');
     }
   }
+
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
