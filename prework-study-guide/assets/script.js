6d42a262ac03a6ecd758729ce6b5203bfcb6d934
PBP66 <53805701+pbp66@users.noreply.github.com>
2022-08-17 21:20:47 -0400

    Implemented listTopics and selectTopic functions

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,19 +1,27 @@
-/*
-if (topic === 'HTML') {
- console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
- console.log("Let's study CSS!");
-} else if (topic === 'Git') {
- console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
- console.log("Let's study JavaScript!");
-} else {
- console.log('Please try again!');
-}
-*/
-
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
+function listTopics() {
+    for (var i = 0; i < topics.length; i++) {
+        console.log(topics[i]);
+    }
+}
+
+/*
 for(var i = 0; i < topics.length; i++) {
     console.log(topics[i]);
    }
+*/
+
+function selectTopic() {
+    if (topics[0] === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topics[0] === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topics[0] === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topics[0] === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
